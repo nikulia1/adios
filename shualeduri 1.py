fa8c451912ce6f8d791d9f034b54f1fbc00645b9
nikulia1 <127594690+nikulia1@users.noreply.github.com>
2023-04-05 20:57:05 +0400

    Add files via upload

---
+++ shualeduri 1.py
@@ -0,0 +1,20 @@
+# python isn't as easy as it looks 3
+
+# class Person:
+#     def __init__(self,age,gender,pulse):
+#         self.age = age
+#         self.gender = gender
+#         self.pulse = pulse
+#     def __str__(self):
+#         return f"age:{self.age} , gender:{self.gende} , pulse:{self.pulse}"
+#     def life(self):
+#         return 26000000/self.pulse/64/24/365
+# p1 = Person(15,"f",20)
+# print(p1.life())
+
+# python isn't as easy as it looks 2
+
+# aii pyrdapir sashinelbaaaa tvini gamecheda verfari ver movifiqre
+
+
+
